34bbc1b261353580f88f3230295d41a6266eab91
Michael Jang <michaeljang86@gmail.com>
2023-07-07 20:07:56 +0000

    Update 3 files

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { readFile, writeFile } from "../helpers/file-helpers";
+
+const DB_PATH = "/src/database.json";
 
 function Home() {
+  let { hits } = JSON.parse(readFile(DB_PATH));
+  hits++;
+  writeFile(DB_PATH, JSON.stringify({ hits }));
+
   return (
     <main>
       <h1>Hello Next!</h1>
+      Welcome! You are visitor number {hits}.
     </main>
   );
 }
